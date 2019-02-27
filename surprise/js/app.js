8fd1d10bb3492fc1272d54cf26e1959d70cf60f3
Serkan Özdemir <s3rkanodemir@gmail.com>
2019-02-27 13:22:21 +0300

    auto play

--- surprise/js/app.js
+++ surprise/js/app.js
@@ -29,7 +29,13 @@ jQuery(function ($) {
 	$('#main-navigation').onePageNav({
 	    filter: ':not(.external)'
 	});
-
+	var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+	if(!isChrome){
+		$('#iframeAudio').remove()
+	}
+else{
+	 $('#playAudio').remove() //just to make sure that it will not have 2x audio in the background 
+}
 	// User define function
 	function Scroll() {
 		var contentTop      =   [];
