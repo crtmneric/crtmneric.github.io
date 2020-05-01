a01d645d200500af74384abfd484a52056201544
Serkan Özdemir <s3rkanodemir@gmail.com>
2020-05-01 12:00:52 +0300

    up

---
+++ sevgilim/js/main.js
@@ -0,0 +1,69 @@
+$(window).load(function(){
+     $('.preloader').fadeOut('slow');
+});
+
+
+/* =Main INIT Function
+-------------------------------------------------------------- */
+function initializeSite() {
+
+	"use strict";
+
+	//OUTLINE DIMENSION AND CENTER
+	(function() {
+	    function centerInit(){
+
+			var sphereContent = $('.sphere'),
+				sphereHeight = sphereContent.height(),
+				parentHeight = $(window).height(),
+				topMargin = (parentHeight - sphereHeight) / 2;
+
+			sphereContent.css({
+				"margin-top" : topMargin+"px"
+			});
+
+			var heroContent = $('.hero'),
+				heroHeight = heroContent.height(),
+				heroTopMargin = (parentHeight - heroHeight) / 2;
+
+			heroContent.css({
+				"margin-top" : heroTopMargin+"px"
+			});
+
+	    }
+
+	    $(document).ready(centerInit);
+		$(window).resize(centerInit);
+	})();
+
+	// Init effect 
+	$('#scene').parallax();
+
+};
+/* END ------------------------------------------------------- */
+
+/* =Document Ready Trigger
+-------------------------------------------------------------- */
+$(window).load(function(){
+
+	initializeSite();
+	(function() {
+		setTimeout(function(){window.scrollTo(0,0);},0);
+	})();
+
+});
+/* END ------------------------------------------------------- */
+
+
+$('#countdown').countdown({
+	date: "May 4, 2020 00:00:00",
+	render: function(data) {
+	  var el = $(this.el);
+	  el.empty()
+	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
+	    .append("<div><h1 style='color:white;'>" + this.leadingZeros(data.days, 2) + " <span>gün</span></h1></div>")
+	    .append("<div><h1 style='color:white;'>" + this.leadingZeros(data.hours, 2) + " <span>saat</span></h1></div>")
+	    .append("<div><h1 style='color:white;'>" + this.leadingZeros(data.min, 2) + " <span>dak</span></h1></div>")
+	    .append("<div><h1 style='color:white;'>" + this.leadingZeros(data.sec, 2) + " <span>sn</span></h1></div>");
+	}
+}); 
