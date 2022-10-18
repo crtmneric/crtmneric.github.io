68568b3ed5771624524e5612a3892eef96f993da
Serkan Özdemir <s3rkanodemir@gmail.com>
2022-10-18 17:02:00 +0300

    me added

---
+++ me/js/calendar.js
@@ -0,0 +1,46 @@
+
+
+const element = document.getElementById("kt_docs_fullcalendar_basic");
+
+var todayDate = moment().startOf("day");
+var YM = todayDate.format("YYYY-MM");
+var YESTERDAY = todayDate.clone().subtract(1, "day").format("YYYY-MM-DD");
+var TODAY = todayDate.format("YYYY-MM-DD");
+var TOMORROW = todayDate.clone().add(1, "day").format("YYYY-MM-DD");
+
+var calendarEl = document.getElementById("kt_docs_fullcalendar_basic");
+var calendar = new FullCalendar.Calendar(calendarEl, {
+    headerToolbar: {
+        left: "prev,next today",
+        center: "title",
+        right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth"
+    },
+
+    height: 800,
+    contentHeight: 780,
+    aspectRatio: 3,  // see: https://fullcalendar.io/docs/aspectRatio
+
+    nowIndicator: true,
+    now: TODAY + "T09:25:00", // just for demo
+
+    views: {
+        dayGridMonth: { buttonText: "month" },
+        timeGridWeek: { buttonText: "week" },
+        timeGridDay: { buttonText: "day" }
+    },
+
+    initialView: "dayGridMonth",
+    initialDate: TODAY,
+
+    editable: true,
+    dayMaxEvents: true, // allow "more" link when too many events
+    navLinks: true,
+    events: [
+        {
+            title: "All Day Event",
+            start: "2020-09-01"
+        },
+    ],
+});
+
+calendar.render();
